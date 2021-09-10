1fa91026d7de95f91298770b4128dbe84d77c415
Vinicius <vini.evangelista@live.com>
2021-09-10 15:08:39 -0300

    changes

---
+++ senai.inlock.webApi/senai.inlock.webApi/Repositories/JogoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi_.Repositories
+{
+    public class JogoRepository
+    {
+        const string STRINGCONEXAO = @"Data source= DESKTOP-DHSRSVI\SQLEXPRESS; initial_catalog= inlock_games_tarde; user Id=sa; pwd=senai@132;";
+
+    }
+}
